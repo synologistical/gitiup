c14d6125009a65b4ae7c7189d74ace97e6d2410f
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-08-30 22:06:58 +0200

    fixup??? object-file.c: use size_t for header lengths
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- object-store-ll.h
+++ object-store-ll.h
@@ -250,10 +250,10 @@ void hash_object_file(const struct git_hash_algo *algo, const void *buf,
 		      size_t len, enum object_type type,
 		      struct object_id *oid);
 
-int write_object_file_flags(const void *buf, unsigned long len,
+int write_object_file_flags(const void *buf, size_t len,
 			    enum object_type type, struct object_id *oid,
 			    unsigned flags);
-static inline int write_object_file(const void *buf, unsigned long len,
+static inline int write_object_file(const void *buf, size_t len,
 				    enum object_type type, struct object_id *oid)
 {
 	return write_object_file_flags(buf, len, type, oid, 0);
